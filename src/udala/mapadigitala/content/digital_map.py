bbd6c239696c324914a736faa27318194c29aa22
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-08-28 15:06:10 +0200

    fields

--- src/udala/mapadigitala/content/digital_map.py
+++ src/udala/mapadigitala/content/digital_map.py
@@ -6,56 +6,60 @@ from plone.dexterity.content import Item
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from udala.mapadigitala import _
+from udala.mapadigitala import _
 
 
 class IDigitalMap(model.Schema):
     """ Marker interface and Dexterity Python Schema for DigitalMap
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    service_name = schema.TextLine(
+        title=_(
+            u'Service name',
+        ),
+        description=_(
+            u'This name will be shown in the upper right corner of the page',
+        ),
+        default=u'',
+        required=False,
+        readonly=False,
+    )
 
-    # model.load('digital_map.xml')
+    shown_widgets = schema.Set(
+        title=_(
+            'Select available widgets',
+        ),
+        value_type=schema.Choice(
+            vocabulary="udala.mapadigitala.widgets",
+        ),
+        required=False,
+        default=set([
+            'Folder',
+            'Image',
+        ]),
+        # defaultFactory=get_default_shown_widgets,
+        readonly=False,
+    )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
-
+    shown_layers = schema.Set(
+        title=_(
+            'Select available map layers',
+        ),
+        value_type=schema.Choice(
+            vocabulary="udala.mapadigitala.layers",
+        ),
+        required=False,
+        default=set([
+            'Folder',
+            'Image',
+        ]),
+        # defaultFactory=get_default_shown_layers,
+        readonly=False,
+    )
 
 @implementer(IDigitalMap)
 class DigitalMap(Item):
