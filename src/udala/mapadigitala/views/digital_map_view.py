fe2c17c66bd828ea627b153e7e4621420d69ff86
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-08-28 15:17:08 +0200

    Add view: view

---
+++ src/udala/mapadigitala/views/digital_map_view.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# from udala.mapadigitala import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class IDigitalMapView(Interface):
+    """ Marker Interface for IDigitalMapView"""
+
+
+@implementer(IDigitalMapView)
+class DigitalMapView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('digital_map_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
